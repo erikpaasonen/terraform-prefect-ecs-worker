c3b36f2709d7d7709597ad6aa74aae5699d4963f
Erik Paasonen <erik.paasonen@121.health>
2025-05-27 15:19:12 -0400

    fix(ci): :green_heart: create unique secret per run to avoid deletion window

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 data "aws_region" "current" {}
 
 resource "aws_secretsmanager_secret" "prefect_api_key" {
-  name                    = "prefect-api-key-${var.name}"
+  name_prefix             = "prefect-api-key-${var.name}-"
   recovery_window_in_days = var.secrets_manager_recovery_in_days
 }
 
