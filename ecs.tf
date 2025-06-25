e2653a7d76242d478453d590b77867e606404853
Erik Paasonen <erik.paasonen@121.health>
2025-06-25 14:50:23 -0400

    chore: :arrow_up: replace newly deprecated syntax

--- ecs.tf
+++ ecs.tf
@@ -42,7 +42,7 @@ resource "aws_ecs_task_definition" "prefect_worker_task_definition" {
         logDriver = "awslogs"
         options = {
           awslogs-group         = aws_cloudwatch_log_group.prefect_worker_log_group.name
-          awslogs-region        = data.aws_region.current.name
+          awslogs-region        = data.aws_region.current.region
           awslogs-stream-prefix = "prefect-worker-${var.name}"
         }
       }
